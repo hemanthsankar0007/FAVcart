24f054184fb6c8104d98dac3a064eb06ca98a72f
ramyaaa28 <157025019+ramyaaa28@users.noreply.github.com>
2025-10-29 07:54:24 +0530

    Fix Header.js avatar and JSX issues - working build

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,26 +1,29 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
-import productsReducer from "./slices/productsSlice";
-import productReducer from './slices/productSlice';
-import authReducer from './slices/authSlice';
-import cartReducer from './slices/cartSlice';
-import orderReducer from './slices/orderSlice';
-import userReducer from './slices/userSlice'
 
+import productsReducer from "./slices/productsSlice";
+import productReducer from "./slices/productSlice";
+import authReducer from "./slices/authSlice";
+import cartReducer from "./slices/cartSlice";
+import orderReducer from "./slices/orderSlice";
+import userReducer from "./slices/userSlice";
 
 const reducer = combineReducers({
-    productsState: productsReducer,
-    productState: productReducer ,
-    authState: authReducer,
-    cartState: cartReducer,
-    orderState: orderReducer,
-    userState: userReducer
-})
-
+  productsState: productsReducer,
+  productState: productReducer,
+  authState: authReducer,
+  cartState: cartReducer,
+  orderState: orderReducer,
+  userState: userReducer,
+});
 
 const store = configureStore({
-    reducer,
-    middleware: [thunk]
-})
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // disables annoying warnings about non-serializable values
+    }).concat(thunk),
+  devTools: process.env.NODE_ENV !== "production", // enables Redux DevTools only in dev
+});
 
 export default store;
